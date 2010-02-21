ad4a597a671921830895140b73f9d27c89accb3b
Daniel Patru <dpatru@gmail.com>
2010-02-21 13:39:58 -0500

    Removed importation of jQuery from bookmarklet.
    JQuery will be imported if necessary from the downloaded file. This way the user will not need to update the bookmark.

--- imageGridBookmarklet.js
+++ imageGridBookmarklet.js
@@ -1,12 +1,7 @@
 // This script loads jQuery if needed and then tries to load the main code from googlecode.
 (function(){
-   var jq = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js';
-   if (!window.jQuery) getScript(jq, get_imagegrid);
-   else get_imagegrid();
-   function get_imagegrid(){
-     var ig = 'http://imagegrid.googlecode.com/hg/imagegrid.js';
-     getScript(ig, null);
-   };
+   var ig = 'http://imagegrid.googlecode.com/hg/imagegrid.js';
+   getScript(ig, null);
    function getScript(url,success){
      var head = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0], done = false;
      var script = document.createElement("script");
